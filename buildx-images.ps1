6d1f151e82527e3bf080a75b86b96714da05cf91
Carles Barreda <9409275+carlesbarreda@users.noreply.github.com>
2020-11-25 00:41:59 +0100

    Added 'latest' tag

--- buildx-images.ps1
+++ buildx-images.ps1
@@ -9,4 +9,4 @@ $IMAGE="${IMAGE_NAME}:${IMAGE_TAG}"
 docker context create --from default ${PROJECT}
 docker buildx create --use --name firebird --driver docker-container ${PROJECT}
 docker buildx use ${PROJECT}
-docker buildx build --platform linux/amd64,linux/386,linux/arm64,linux/arm/v7 --tag ${IMAGE} --push --file ./Dockerfile .
+docker buildx build --platform linux/amd64,linux/386,linux/arm64,linux/arm/v7 --tag ${IMAGE} --tag "${IMAGE_NAME}:latest" --push --file ./Dockerfile .
